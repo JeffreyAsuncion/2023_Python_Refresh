a9128255acd91aaa6ccc90ae4f497c30d57bf23c
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2023-01-26 11:34:40 -0500

    face_dection working as intended, will add comments for clarity and next steps

---
+++ 115_face_detection.py
@@ -0,0 +1,20 @@
+# pip3 install opencv-python
+
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+img = cv2.imread('test.jpg')
+
+# need to convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    
+cv2.imshow('img', img)
+cv2.waitKey()
+
+cv2.imwrite("face_detected.jpg", img)
